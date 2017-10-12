04a54be6019b7b2abd3b6b6e9c1a616ca7fd5358
Will Long <will@kerasai.com>
2017-10-11 21:52:43 -0500

    CRM-21296: built-out `civicrm_member_roles` settings form

--- modules/civicrm_member_roles/src/Form/SettingsForm.php
+++ modules/civicrm_member_roles/src/Form/SettingsForm.php
@@ -11,7 +11,7 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    ['civicrm_member_roles.settings'];
+    return ['civicrm_member_roles.settings'];
   }
 
   /**
@@ -25,6 +25,36 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('civicrm_member_roles.settings');
+
+    $form['settings'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Settings'),
+    ];
+
+    $form['settings']['sync_method'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Automatic Synchronization Method'),
+      '#description' => $this->t('Select which method CiviMember Roles Sync will use to automatically synchronize Memberships and Roles. If you choose user login/logout, you will have to run an initial "Manual Synchronization" after you create a new rule for it to be applied to all users and contacts. If you do not select an option, automatic synchronization will be disabled. You will have to use the "Manually Synchronize" form to synchronize memberships and roles yourself. Leave the default setting if you are unsure which method to use.'),
+      '#default_value' => $config->get('sync_method'),
+      '#options' => [
+        'login' => $this->t('Synchronize whenever a user logs in or logs out. This action is performed only on the user logging in or out.'),
+        'cron' => $this->t('Synchronize when Drupal cron is ran. This action will be performed on all users and contacts.'),
+        'update' => $this->t('Synchronize when membership is updated.'),
+      ],
+    ];
+
+    $form['settings']['cron_limit'] = [
+      '#type' => 'number',
+      '#title' => t('Memberships Synchronized on Cron'),
+      '#description' => t('Enter how many Memberships and Roles you would like to synchronize per cron run. Synchronization will be performed randomly. This prevents the operation from timing out when too many items are processed at once. If this is empty, all Memberships and Roles will be processed.'),
+      '#default_value' => $config->get('cron_limit'),
+      '#min' => 0,
+      '#step' => 1,
+      '#size' => 15,
+      '#maxlength' => 4,
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -32,6 +62,12 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('civicrm_member_roles.settings');
+
+    $config->set('sync_method', $form_state->getValue('sync_method'));
+    $config->set('cron_limit', $form_state->getValue('cron_limit'));
+    $config->save();
+
     parent::submitForm($form, $form_state);
   }
 
